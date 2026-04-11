c09faea794d00b0431c21e54512dbb4f47657414
Ali Muksin <ali.muksin0510@gmail.com>
2026-04-12 03:49:17 +0700

    update: main node execution logic to test initial interface

--- nodes/npilot/Npilot.node.ts
+++ nodes/npilot/Npilot.node.ts
@@ -14,45 +14,44 @@ export class Npilot implements INodeType {
 		usableAsTool: true,
 	};
 
-	// The function below is responsible for actually doing whatever this node
-	// is supposed to do. In this case, we're just appending the `myString` property
-	// with whatever the user has entered.
-	// You can make async calls and use `await`.
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-		const items = this.getInputData();
+		const items = this.getInputData(); // get all items from previous node
+		const results: INodeExecutionData[] = [];
 
-		let item: INodeExecutionData;
-		let myString: string;
-
-		// Iterates over all input items and add the key "myString" with the
-		// value the parameter "myString" resolves to.
-		// (This could be a different value for each item in case it contains an expression)
-		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+		for (let i = 0; i < items.length; i++) {
 			try {
-				myString = this.getNodeParameter('myString', itemIndex, '') as string;
-				item = items[itemIndex];
+				this.logger.info('items data', items[i].json);
+				// get the user input
+				const opts = {
+					session: this.getNodeParameter('session', i),
+					sessionID: this.getNodeParameter('sessionID', i),
+					mode: this.getNodeParameter('mode', i),
+				};
 
-				item.json.myString = myString;
+				// add all to the resuls
+				results.push({
+					json: {
+						...items[i].json,
+						...opts,
+					},
+					pairedItem: { item: i },
+				});
 			} catch (error) {
-				// This node should never fail but we want to showcase how
-				// to handle errors.
 				if (this.continueOnFail()) {
-					items.push({ json: this.getInputData(itemIndex)[0].json, error, pairedItem: itemIndex });
+					results.push({
+						json: { ...items[i].json, error: (error as Error).message },
+						pairedItem: i,
+					});
 				} else {
-					// Adding `itemIndex` allows other workflows to handle this error
 					if (error.context) {
-						// If the error thrown already contains the context property,
-						// only append the itemIndex
-						error.context.itemIndex = itemIndex;
+						error.context.itemIndex = i;
 						throw error;
 					}
-					throw new NodeOperationError(this.getNode(), error, {
-						itemIndex,
-					});
+					throw new NodeOperationError(this.getNode(), error as Error, { itemIndex: i });
 				}
 			}
 		}
 
-		return [items];
+		return [results];
 	}
 }
