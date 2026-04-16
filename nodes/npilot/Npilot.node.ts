ba8a8ae864aa152f06649c2b2be622e6f9ee5e23
Ali Muksin <ali.muksin0510@gmail.com>
2026-04-17 02:11:14 +0700

    update: add initial logic to get additional option parameter and check browser session

--- nodes/npilot/Npilot.node.ts
+++ nodes/npilot/Npilot.node.ts
@@ -6,6 +6,9 @@ import type {
 } from 'n8n-workflow';
 import { NodeOperationError } from 'n8n-workflow';
 import descriptions from './descriptions';
+import { sessionManager } from './session/SessionManager';
+import type { BrowserType } from './session/types';
+// import { ActionManager } from './action/ActionManager';
 
 export class Npilot implements INodeType {
 	description: INodeTypeDescription = {
@@ -20,19 +23,64 @@ export class Npilot implements INodeType {
 
 		for (let i = 0; i < items.length; i++) {
 			try {
-				this.logger.info('items data', items[i].json);
-				// get the user input
-				const opts = {
-					session: this.getNodeParameter('session', i),
-					sessionID: this.getNodeParameter('sessionID', i),
-					mode: this.getNodeParameter('mode', i),
+				// get all additional options
+				const options = this.getNodeParameter('options', i, {}) as {
+					browserType?: BrowserType;
+					extraHTTPHeaders?: {
+						headerValues?: Array<{
+							name: string;
+							value: string;
+						}>;
+					};
+					headless?: boolean;
+					ttlms?: number;
+					userAgent?: string;
+					viewport?: { height: number; width: number };
 				};
+
+				// build compatible sructure extra header for Playwright
+				const extraHTTPHeaders: Record<string, string> = {};
+				const entries = options.extraHTTPHeaders?.headerValues ?? [];
+				if (entries.length) {
+					for (const entry of entries) {
+						if (entry.name) {
+							extraHTTPHeaders[entry.name] = entry.value;
+						}
+					}
+				}
+
+				// get session value
+				const session = this.getNodeParameter('session', i, '') as 'new' | 'previous';
+				let sessionId;
+
+				// check session
+				if (session === 'previous') {
+					sessionId = this.getNodeParameter('sessionId', i, '') as string;
+					if (!sessionId) {
+						throw new NodeOperationError(
+							this.getNode(),
+							'Session ID not found. Ensure a Browser Open node runs before this node.',
+							{ itemIndex: i },
+						);
+					}
+				}
+
+				// open new browser
+				if (session == 'new') {
+					this.logger.info('options', options);
+					sessionId = await sessionManager.openSession({
+						...options,
+						extraHTTPHeaders,
+					});
+				}
 
 				// add all to the resuls
 				results.push({
 					json: {
 						...items[i].json,
-						...opts,
+						...{ ...options, extraHTTPHeaders },
+						session,
+						sessionId,
 					},
 					pairedItem: { item: i },
 				});
