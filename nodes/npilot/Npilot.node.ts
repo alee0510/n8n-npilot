682067424d41b7afcaeb91f9b58c64a8b97e51bf
Ali Muksin <ali.muksin0510@gmail.com>
2026-04-17 11:50:13 +0700

    update: implement step action for each action.type

--- nodes/npilot/Npilot.node.ts
+++ nodes/npilot/Npilot.node.ts
@@ -6,9 +6,9 @@ import type {
 } from 'n8n-workflow';
 import { NodeOperationError } from 'n8n-workflow';
 import descriptions from './descriptions';
-// import { sessionManager } from './session/SessionManager';
+import { sessionManager } from './session/SessionManager';
+import { ActionManager } from './action/ActionManager';
 import type { AdditionalOptions, Step } from './types/Npilot.types';
-// import { ActionManager } from './action/ActionManager';
 
 export class Npilot implements INodeType {
 	description: INodeTypeDescription = {
@@ -39,48 +39,146 @@ export class Npilot implements INodeType {
 
 				// get session value
 				const session = this.getNodeParameter('session', i, '') as 'new' | 'previous';
-				// let sessionId = '';
+				let sessionId = '';
 
 				// check session
-				// if (session === 'previous') {
-				// 	sessionId = this.getNodeParameter('sessionId', i, '') as string;
-				// 	if (!sessionId) {
-				// 		throw new NodeOperationError(
-				// 			this.getNode(),
-				// 			'Session ID not found. Ensure a Browser Open node runs before this node.',
-				// 			{ itemIndex: i },
-				// 		);
-				// 	}
-				// }
+				if (session === 'previous') {
+					sessionId = this.getNodeParameter('sessionId', i, '') as string;
+					if (!sessionId) {
+						throw new NodeOperationError(
+							this.getNode(),
+							'Session ID not found. Ensure a Browser Open node runs before this node.',
+							{ itemIndex: i },
+						);
+					}
+				}
 
 				// open new browser
-				// if (session == 'new') {
-				// 	this.logger.info('options', options);
-				// 	sessionId = await sessionManager.openSession({
-				// 		...options,
-				// 		extraHTTPHeaders,
-				// 	});
-				// }
+				if (session == 'new') {
+					this.logger.info('options', options);
+					sessionId = await sessionManager.openSession({
+						...options,
+						extraHTTPHeaders,
+					});
+				}
 
 				// get page
-				// const page = sessionManager.getPage(sessionId);
-				// const outputField = this.getNodeParameter('outputField', i, 'result') as string;
-				// const actionResult = {};
+				const page = sessionManager.getPage(sessionId);
 
 				// get the action parameter
-				const step = this.getNodeParameter('steps.step', i, []) as Array<Step>;
+				const steps = this.getNodeParameter('steps.step', i, []) as Array<Step>;
+				const action = new ActionManager(page);
+
+				// do all action
+				let actionResult: unknown;
+				for (const step of steps) {
+					switch (step.action) {
+						case 'navigate':
+							actionResult = await action.navigate({
+								url: step.url || '',
+								waitUntil: step.waitUntil,
+								timeout: step.timeout,
+							});
+							break;
+						case 'check':
+							actionResult = await action.check({
+								selector: step.selector || '',
+								checked: step.checked || true,
+							});
+							break;
+						case 'click':
+							actionResult = await action.click({
+								selector: step.selector || '',
+								timeout: step.timeout || 1000,
+							});
+							break;
+						case 'closeSession':
+							actionResult = await sessionManager.closeSession(sessionId);
+							break;
+						case 'evaluate':
+							actionResult = await action.evaluate({
+								expression: step.expression || '',
+							});
+							break;
+						case 'extractAttribute':
+							actionResult = await action.extractAttribute({
+								selector: step.selector || '',
+								attribute: step.attributeName || '',
+							});
+							break;
+						case 'extractTable':
+							break;
+						case 'extractText':
+							actionResult = await action.extractText({
+								selector: step.selector || '',
+							});
+							break;
+						case 'hover':
+							actionResult = await action.hover({
+								selector: step.selector || '',
+							});
+							break;
+						case 'pressKey':
+							actionResult = await action.pressKey({
+								key: step.key || '',
+							});
+							break;
+						case 'screenshot':
+							actionResult = await action.screenshot({
+								fullPage: step.fullPage || false,
+							});
+							break;
+						case 'scrollTo':
+							actionResult = await action.scrollTo({
+								selector: step.selector || '',
+							});
+							break;
+						case 'select':
+							actionResult = await action.select({
+								selector: step.selector || '',
+								value: step.selectValue || '',
+							});
+							break;
+						case 'type':
+							actionResult = await action.type({
+								selector: step.selector || '',
+								text: step.text || '',
+								clearFirst: step.clearFirst || false,
+							});
+							break;
+						case 'waitForNavigation':
+							actionResult = await action.waitForNavigation({
+								url: step.url || '',
+								timeout: step.timeout || 1000,
+							});
+							break;
+						case 'waitForSelector':
+							actionResult = await action.waitForSelector({
+								selector: step.selector || '',
+								state: step.waitState || 'visible',
+								timeout: step.timeout || 1000,
+							});
+							break;
+						case 'waitForTimeout':
+							actionResult = await action.waitForTimeout({
+								timeout: step.timeout || 1000,
+							});
+							break;
+						default:
+							actionResult = await sessionManager.closeAll();
+					}
+
+					// store reesult if action return data
+					if (actionResult !== undefined && step.outputField) {
+						items[i].json[step.outputField] = actionResult;
+					}
+				}
 
 				// add all to the resuls
 				results.push({
 					json: {
 						...items[i].json,
-						...{ ...options, extraHTTPHeaders },
-						session,
-						// sessionId,
-						// page,
-						// outputField,
-						// actionResult,
-						step,
+						sessionId,
 					},
 					pairedItem: { item: i },
 				});
