783ef8e163acd5a0918dcf0a1fa9453c46d88587
Ali Muksin <ali.muksin0510@gmail.com>
2026-04-17 10:58:59 +0700

    upsert: add and test step parameter

--- nodes/npilot/Npilot.node.ts
+++ nodes/npilot/Npilot.node.ts
@@ -6,8 +6,8 @@ import type {
 } from 'n8n-workflow';
 import { NodeOperationError } from 'n8n-workflow';
 import descriptions from './descriptions';
-import { sessionManager } from './session/SessionManager';
-import type { BrowserType } from './session/types';
+// import { sessionManager } from './session/SessionManager';
+import type { AdditionalOptions, Step } from './types/Npilot.types';
 // import { ActionManager } from './action/ActionManager';
 
 export class Npilot implements INodeType {
@@ -24,19 +24,7 @@ export class Npilot implements INodeType {
 		for (let i = 0; i < items.length; i++) {
 			try {
 				// get all additional options
-				const options = this.getNodeParameter('options', i, {}) as {
-					browserType?: BrowserType;
-					extraHTTPHeaders?: {
-						headerValues?: Array<{
-							name: string;
-							value: string;
-						}>;
-					};
-					headless?: boolean;
-					ttlms?: number;
-					userAgent?: string;
-					viewport?: { height: number; width: number };
-				};
+				const options = this.getNodeParameter('options', i, {}) as AdditionalOptions;
 
 				// build compatible sructure extra header for Playwright
 				const extraHTTPHeaders: Record<string, string> = {};
@@ -51,28 +39,36 @@ export class Npilot implements INodeType {
 
 				// get session value
 				const session = this.getNodeParameter('session', i, '') as 'new' | 'previous';
-				let sessionId;
+				// let sessionId = '';
 
 				// check session
-				if (session === 'previous') {
-					sessionId = this.getNodeParameter('sessionId', i, '') as string;
-					if (!sessionId) {
-						throw new NodeOperationError(
-							this.getNode(),
-							'Session ID not found. Ensure a Browser Open node runs before this node.',
-							{ itemIndex: i },
-						);
-					}
-				}
+				// if (session === 'previous') {
+				// 	sessionId = this.getNodeParameter('sessionId', i, '') as string;
+				// 	if (!sessionId) {
+				// 		throw new NodeOperationError(
+				// 			this.getNode(),
+				// 			'Session ID not found. Ensure a Browser Open node runs before this node.',
+				// 			{ itemIndex: i },
+				// 		);
+				// 	}
+				// }
 
 				// open new browser
-				if (session == 'new') {
-					this.logger.info('options', options);
-					sessionId = await sessionManager.openSession({
-						...options,
-						extraHTTPHeaders,
-					});
-				}
+				// if (session == 'new') {
+				// 	this.logger.info('options', options);
+				// 	sessionId = await sessionManager.openSession({
+				// 		...options,
+				// 		extraHTTPHeaders,
+				// 	});
+				// }
+
+				// get page
+				// const page = sessionManager.getPage(sessionId);
+				// const outputField = this.getNodeParameter('outputField', i, 'result') as string;
+				// const actionResult = {};
+
+				// get the action parameter
+				const step = this.getNodeParameter('steps.step', i, []) as Array<Step>;
 
 				// add all to the resuls
 				results.push({
@@ -80,7 +76,11 @@ export class Npilot implements INodeType {
 						...items[i].json,
 						...{ ...options, extraHTTPHeaders },
 						session,
-						sessionId,
+						// sessionId,
+						// page,
+						// outputField,
+						// actionResult,
+						step,
 					},
 					pairedItem: { item: i },
 				});
